66052716920bcd7500749ee9a63b2b1337700484
Allaniyaz <a_aleke@inbox.ru>
2021-11-26 16:37:10 +0500

    created migrations

---
+++ database/migrations/2021_11_26_113008_create_table_words.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTableWords extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('words', function (Blueprint $table) {
+            $table->id();
+            $table->string('title', 1000);
+            $table->integer('checksum');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('words');
+    }
+}
